0fc81118699606158ceb4f82f6699c53785d8bf0
Zorgit <piligrimexe@Gmail.com>
2025-08-08 00:51:18 +0300

    add test informaiton in welcom controller

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,5 +1,8 @@
 package hexlet.code.app.controller;
 
+import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.utill.RandomUsers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/welcome")
 public class WelcomeController {
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String welcome() {
-        return "Welcome to Spring";
+        var randomUsers = RandomUsers.generateFakeUsers(5);
+        userRepository.saveAll(randomUsers);
+        var users = userRepository.findAll();
+        return "Welcome to Spring" + users.toString();
     }
+
 }
