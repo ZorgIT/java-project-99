dac26e87a902a8514d854d075a61f856cc92c12c
Zorgit <piligrimexe@Gmail.com>
2025-08-11 23:02:27 +0300

    update random user util

--- src/main/java/hexlet/code/app/utill/RandomUsers.java
+++ src/main/java/hexlet/code/app/utill/RandomUsers.java
@@ -5,6 +5,7 @@ import hexlet.code.app.repository.UserRepository;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +20,12 @@ public class RandomUsers {
             user.setLastName(faker.name().lastName());
             user.setEmail(faker.internet().emailAddress());
             user.setPassword(faker.internet().password(8, 16));
+            user.setCreatedAt(LocalDateTime.now());
+            user.setUpdatedAt(LocalDateTime.now());
+            user.setModifiedBy(faker.name().username() + "test");
+            user.setCreatedBy(faker.name().username() + "test");
             users.add(user);
+
         }
 
 
