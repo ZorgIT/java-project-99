f56edf2a8b120f1f40ed5ef813ba8afe6e155458
Zorgit <piligrimexe@Gmail.com>
2025-08-14 22:59:07 +0300

    Change auth to username from email

--- src/main/java/hexlet/code/app/dto/AuthRequest.java
+++ src/main/java/hexlet/code/app/dto/AuthRequest.java
@@ -1,13 +1,12 @@
 package hexlet.code.app.dto;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class AuthRequest {
-    private String email;
+    private String username;
     private String password;
 }
