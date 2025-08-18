75aecc55f475acbfd6370a577aace00648bc2c75
Zorgit <piligrimexe@Gmail.com>
2025-08-18 23:05:18 +0300

    Getter setter added

--- src/main/java/hexlet/code/app/dto/TaskUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/TaskUpdateDTO.java
@@ -2,7 +2,11 @@ package hexlet.code.app.dto;
 
 import hexlet.code.app.model.TaskStatus;
 import hexlet.code.app.model.User;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TaskUpdateDTO {
     private String name;
     private Integer index;
