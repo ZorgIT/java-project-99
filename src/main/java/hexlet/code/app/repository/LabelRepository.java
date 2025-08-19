62a6e9288f80ae01f00879fd5ea6dbfee3976ece
Zorgit <piligrimexe@Gmail.com>
2025-08-19 19:18:27 +0300

    Add find by name method

--- src/main/java/hexlet/code/app/repository/LabelRepository.java
+++ src/main/java/hexlet/code/app/repository/LabelRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LabelRepository extends JpaRepository<Label, Long> {
     Boolean existsByName(String name);
+
+    Label findByName(String name);
 }
