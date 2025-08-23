a445d3995fed3a997fb07f79453caff1acb9fe34
Zorgit <piligrimexe@Gmail.com>
2025-08-23 17:32:03 +0300

    User correction

--- src/main/java/hexlet/code/dto/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/UserCreateDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserCreateDTO {
 
     @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
